12f7df49ce4463be05b4945c61ce6d6a2787575c
cwarkk <73570686+cwarkk@users.noreply.github.com>
2021-10-04 20:56:28 +0600

    initial commit

---
+++ app/src/test/java/com/example/android/guesstheword/ExampleUnitTest.kt
@@ -0,0 +1,10 @@
+package com.example.android.guesstheword
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
